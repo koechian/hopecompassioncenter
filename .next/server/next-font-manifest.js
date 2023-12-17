2c6623890a8268145d565b6b2f8f639a52822320
Ian Koech <ianovic8@live.com>
2023-12-17 12:15:53 +0300

    Remove .next from version control

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/nelsonn/react-apps/hopecenter/app/layout\":[\"static/media/c9a5bc6a7c948fb0-s.p.woff2\",\"static/media/e9c96f72b3732f88-s.p.woff2\",\"static/media/c9a5bc6a7c948fb0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Jp3vnkcBFV3nobjVZZ8qmdfcRCbFOuUb1/GWJeJH8zM=\"\n}"
